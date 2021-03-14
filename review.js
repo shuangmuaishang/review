305e749c7853b7249481aa9b7e685fd00f229ed7
xiaobao <609899207@qq.com>
2021-03-14 15:18:54 +0800

    change

--- review.js
+++ review.js
@@ -224,16 +224,31 @@ Promise.race = function (iterators) {
             })
         }
     })
-
 }
-var promise1 = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 500, 'one');
-});
+// var promise1 = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 500, 'one');
+// });
 
-var promise2 = new Promise(function(resolve, reject) {
-    setTimeout(resolve, 100, 'two');
-});
-Promise.race([promise1, promise2]).then(function(value) {
-    console.log(value);
-    // Both resolve, but promise2 is faster
-});``
+// var promise2 = new Promise(function(resolve, reject) {
+//     setTimeout(resolve, 100, 'two');
+// });
+// Promise.race([promise1, promise2]).then(function(value) {
+//     console.log(value);
+//     // Both resolve, but promise2 is faster
+// });
+
+class Axios { 
+    constructor() {} 
+    request(config) { 
+        return new Promise(resolve => { 
+            const {url = '', method = 'get', data = {}} = config; 
+            // 发送ajax请求 
+            const xhr = new XMLHttpRequest(); 
+            xhr.open(method, url, true); 
+            xhr.onload = function() { 
+                resolve(xhr.responseText); 
+            } 
+            xhr.send(data); 
+        }) 
+    } 
+} 
